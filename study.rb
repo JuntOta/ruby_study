439bc5b012e4a8bcd3252b10daa6a7ef39c81577
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-06 19:54:37 +0900

    ハッシュ再代入

--- study.rb
+++ study.rb
@@ -1,5 +1,16 @@
-# 変数examにハッシュを代入してください
-exam = {subject:Math, score:80}
+exam = {"subject" => "Math", "score" => 80}
 
-# 変数examを出力してください
-puts exam
+# キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キー「"subject"」の値を「"Science"」に更新してください
+exam["subject"] = "Science"
+
+# もう一度、キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キーが「"grade"」、値が「"good"」の要素を追加してください
+exam["grade"] = "good"
+
+# キー「"grade"」の値を出力してください
+puts exam["grade"]
