2a946e3855adeb35a0ca7b6bc57e39467a232bad
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-06 19:44:08 +0900

    スコープ

--- study.rb
+++ study.rb
@@ -1,5 +1,11 @@
 languages = ["日本語", "英語", "スペイン語"]
+border = "---------------------"
 
+languages.each do |language|
+  # 変数borderを出力してください
+  puts border
+  puts "#{language}を話せます"
+end
 # each文を用いて、要素ごとに「○○を話せます」と出力してください
 languages.each do |language|
   puts "#{language}を話せます"
