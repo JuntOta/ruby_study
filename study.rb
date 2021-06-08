8249c634464a4ccbc6bc7c0fb61dcd327968e633
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-08 23:00:46 +0900

    インスタンス変数

--- study.rb
+++ study.rb
@@ -1,5 +1,15 @@
 class Menu
-  # nameとpriceというインスタンス変数を用意してください
   attr_accessor :name
   attr_accessor :price
 end
+
+menu1 = Menu.new
+
+menu1.name = "ピザ"
+puts menu1.name
+
+# menu1のpriceに「800」を代入してください
+menu1.price = 800
+
+# menu1のpriceを出力してください
+puts menu1.price
