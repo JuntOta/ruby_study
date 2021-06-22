d4f48004c6ad09be43db8b1f00473bc5d2b434a7
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-22 20:42:37 +0900

    メニューに番号表示

--- study.rb
+++ study.rb
@@ -5,10 +5,15 @@ menu2 = Menu.new(name: "すし", price: 1000)
 menu3 = Menu.new(name: "コーラ", price: 300)
 menu4 = Menu.new(name: "お茶", price: 200)
 
-# 変数menusを定義して配列を代入してください
-menus = [menu1,menu2,menu3,menu4]
+menus = [menu1, menu2, menu3, menu4]
 
-# menusに対して繰り返し処理を実行してください
+# 変数indexを定義して「0」を代入してください
+index = 0
+
 menus.each do |menu|
-  puts menu.info
+  # 番号をつけてメニューの内容が出力されるように書き換えてください
+  puts "#{index}. " + menu.info
+  
+  # 変数indexに1を加えて値を更新してください
+  index += 1
 end
