4a4db1fac1657d7a7b5a440735d90d0709974b17
JuntOta <junto.ota@ezweb.ne.jp>
2021-07-01 12:28:23 +0900

    文字列の出力

--- study.java
+++ study.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Java");
+    
+    // この文章をコメントにしてください
+    
+    // 「こんにちは、Java」と出力してください
+    System.out.println("こんにちは、Java");
+    
   }
 }
+
