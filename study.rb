3264133676d76ca056fc3fd7ffc1a2025f1d3c72
JuntOta <junto.ota@ezweb.ne.jp>
2021-07-02 21:13:08 +0900

    条件分岐

--- study.rb
+++ study.rb
@@ -1,12 +1,9 @@
-def check_name(str) 
-  if str.include?(".")
-    puts "!エラー!記号は登録できません"
-  elsif str.include?(" ")
-    puts "!エラー!空白は登録できません"
-  else
-    puts "登録が完了しました"
-  end
+input = gets.to_i
+
+if input <= 0
+  puts "0以下の数字です"
+elsif input <= 10
+  puts "10以下の数字です"
+else
+  puts "10より大きい数字です"
 end
-puts "登録したい名前を入力してください(例)YamadaTaro"
-str = gets
-check_name(str) 
